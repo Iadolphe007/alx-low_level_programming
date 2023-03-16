18e8c63da421c5d7fe67e55b24ed085f86c29bc5
Iadolphe007 <iadolphe007@gmail.com>
2023-03-16 10:44:45 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- * main - entry point
- *
- * Return: setted return value
+ * main - a program taht provide teh error
+ * Return:if teh function succeeded the return value is one
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar,");
-	fputs("2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
-
 }
