175ff32ce47504f14b565bcb0e7d90159cb76e2d
Iadolphe007 <iadolphe007@gmail.com>
2023-03-16 08:37:04 -0700

    size of computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - used function to print size of computer program
+ *
+ * Return: return walue is assigned to zero
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%u\n", sizeof(char));
+	printf("%lu\n", sizeof(char));
+	printf("%lu", sizeof(char));
+	return (0);
+}
