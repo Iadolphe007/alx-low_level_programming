7e16d4902ce28da4512cbcb2ba46cf4513aebc33
Iadolphe007 <iadolphe007@gmail.com>
2023-03-16 08:41:57 -0700

    size of computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 	printf("%lu\n", sizeof(char));
-	printf("%u\n", sizeof(char));
-	printf("%lu\n", sizeof(char));
-	printf("%lu", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu", sizeof(double));
 	return (0);
 }
