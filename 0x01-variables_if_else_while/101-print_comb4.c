6e3fe24e6a440f11d59fc737937591a999194be1
Iadolphe007 <iadolphe007@gmail.com>
2023-03-17 22:06:47 +0000

    triple combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if (i != j && i != k && k != j)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i + j + k != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
