8bf0a936fb5bf6b63ffc13daedc916e6b9561c12
Iadolphe007 <iadolphe007@gmail.com>
2023-03-17 14:47:56 +0000

    upper and lower case letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - function returning lower case and upper case alphabet
+ * Return: always set to zero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
