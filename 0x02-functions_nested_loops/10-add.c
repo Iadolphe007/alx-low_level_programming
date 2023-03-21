cb10c52d0468a2ffeb3fe51e19f7d7c279e26823
Iadolphe007 <iadolphe007@gmail.com>
2023-03-21 19:00:05 +0000

    adding parameters

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function adding to number
+ * @a: first number
+ * @b: second number
+ * Return: sum of the two number
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
