e9c60a986f722b3fe00c7980d50a1ff557d0b90d
Iadolphe007 <iadolphe007@gmail.com>
2023-03-21 16:04:33 +0000

    printing lower case letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - function returning lower number
+ * Return: 1 if lowercase else 0
+ * @c: any character to be printed
+ */
+
+int _islower(int c)
+{
+
+	if (c > 97 && c < 122)
+		return (1);
+	else
+		return (0);
+}
