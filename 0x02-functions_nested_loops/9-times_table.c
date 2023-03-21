1863ce232ff3eeff13ae9ddaec0e2a30ecf2e3a0
Iadolphe007 <iadolphe007@gmail.com>
2023-03-21 20:41:00 +0000

    multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * times_table - print multiplication table
+ * Return: always zero
+ */
+
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+	int u;
+	int d;
+
+	for (x = 0; x <= 0; x++)
+	{
+		for (y = 0; y <= 0; y++)
+		{
+			z = x * y;
+			if (z > 0)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+			if (y != 0)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
+			_putchar(z + '0');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
