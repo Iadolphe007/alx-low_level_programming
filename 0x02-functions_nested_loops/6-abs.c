2f8859f8e4d09c2e79342d95cd6cdd5b97d7260b
Iadolphe007 <iadolphe007@gmail.com>
2023-03-21 17:50:54 +0000

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * _abs - functionto return the absolute value
+ * @a: parameter
+ * Return: always positive number
+ */
+
+
+
+int _abs(int a)
+{
+	int a;
+
+	if (a < 0)
+		a = -(a);
+	else if
+		a = 1;
+	return (a);
+}
