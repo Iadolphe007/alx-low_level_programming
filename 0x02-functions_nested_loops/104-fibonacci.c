9f2e044c71225d1e727288f8392a1230310c454d
Iadolphe007 <iadolphe007@gmail.com>
2023-03-22 11:09:58 +0000

    first 98 fibonnaci number

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print 98 fibonnaci number
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	a = 0;
+	b = 1;
+	c = 2;
+
+	while (a < 98)
+	{
+		if (a == 0)
+			printf("%d", b);
+		else if (a == 1)
+			printf(", %d", c)
+		else
+		{
+			c += b;
+			b = c - b;
+			printf(", %d", c);
+		}
+	}
+	printf("\n");
+	return (0);
+}
