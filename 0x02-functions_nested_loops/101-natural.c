2cf5eb7e2715a179cde4335a2b9871a0ea152277
Iadolphe007 <iadolphe007@gmail.com>
2023-03-22 09:27:20 +0000

    multiple of the number

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - compute multiple of a number
+ * Return: always 0
+ */
+
+
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	for (i = 3; i < 1024 i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+		else
+			sum = sum
+
+	}
+	printf("%d\n", sum);
+	return (0);
+}
