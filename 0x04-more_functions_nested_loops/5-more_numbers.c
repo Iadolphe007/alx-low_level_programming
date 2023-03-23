8a4dbfba2d64685efa9f5a95f89cb2777980b5a1
Iadolphe007 <iadolphe007@gmail.com>
2023-03-23 14:45:32 +0000

    printing column number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,16 +11,14 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	j = 0;
-
-	while (j < 10)
+	for (j = 1; j <= 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i + '0');
+			if (i >= 10)
+				_putchar('1');
+			_putchar (i % 10 + '0');
 		}
 		_putchar('\n');
-		j++;
 	}
-	_putchar('\n');
 }
