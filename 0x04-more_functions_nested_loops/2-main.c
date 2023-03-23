6cae8431e7649eb4a52f08c9bfa48f3bd03490a8
Iadolphe007 <iadolphe007@gmail.com>
2023-03-23 12:23:24 +0000

    product of two

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -check the code
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%d\n", mul(2, 24));
+	printf("%d\n", mul(-12, 2));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul- function returning multiple of two number
+ * @a: first character
+ * @b: second character
+ * Return: product of the two number
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
