01d784bc93ae80157b087cf018ada53902a49307
Iadolphe007 <iadolphe007@gmail.com>
2023-03-27 12:33:01 +0000

    printing sting line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - function printing a string followed by a new line
+ * @str: a string to be printed out in stdout
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
