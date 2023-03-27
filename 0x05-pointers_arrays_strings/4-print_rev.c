3ba71aa428695b935c40825a517911424708f666
Iadolphe007 <iadolphe007@gmail.com>
2023-03-27 13:20:25 +0000

    printing reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/** 
+ * print_rev - printing reverse string
+ * @s: string print out reverse
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int len;
+
+	len = strlen(s);
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
