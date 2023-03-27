268b6f5077b81284fd9509d6fa3df7a166e1a632
Iadolphe007 <iadolphe007@gmail.com>
2023-03-27 16:45:12 +0000

    printing first character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,17 @@
 void puts2(char *str)
 {
 	int i;
+	int j;
 
-	i = 0;
+	j = 0;
 
-	while (str[i] != '\0')
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
 	_putchar('\n');
 }
