06a39dc31513145ed366b402d7372fd342e0ef47
Iadolphe007 <iadolphe007@gmail.com>
2023-03-27 16:28:06 +0000

    elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i]);
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
