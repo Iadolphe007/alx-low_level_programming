4f1ce99b10d53015c1c7e93a063691ca6c0633b8
Iadolphe007 <iadolphe007@gmail.com>
2023-03-30 13:07:22 +0000

    make magic happen

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,19 @@
 #include "main.h"
-
-
 #include <stdio.h>
+
+/**
+ * main - compute numbers
+ * Return: always 0
+ */
 
 int main(void)
 {
-       	int n;
+	int n;
 	int a[5];
 	int *p;
 
 	a[2] = 1024;
 	p = &n;
-
 	/*
 	 * write your line of code here...
 	 * Remember:
