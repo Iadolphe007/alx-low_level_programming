9e35e170330ae9a07dbfb25ee5b7ef8499799b32
Iadolphe007 <iadolphe007@gmail.com>
2023-03-30 07:44:08 +0000

    string concatonation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,17 +15,12 @@ char *_strncat(char *dest, char *src, int n)
 	destlen = 0;
 	srclen = 0;
 
+
 	for (i = 0; dest[i] != '\0'; i++)
-	{
 		destlen++;
-	}
 	for (i = 0; src[i] != '\0'; i++)
-	{
-	       srclen++;
-	}
+		srclen++;
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[destlen + i] = src[i];
-	}
 	return (dest);
 }
