4efb629b49c46a5fe28ba2f462e60fa78f357375
Iadolphe007 <iadolphe007@gmail.com>
2023-03-30 06:19:32 +0000

    concatenated two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,11 @@
 #include "main.h"
-
-
+/**
+ * _strncat - function that concatenates two strings
+ * @src: source string
+ * @dest: destination string
+ * @n: number to be concatonated
+ * Return: destination string
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	char destlen;
@@ -11,10 +16,16 @@ char *_strncat(char *dest, char *src, int n)
 	srclen = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
+	{
 		destlen++;
+	}
 	for (i = 0; src[i] != '\0'; i++)
+	{
 	       srclen++;
+	}
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[destlen + i] = src[i];
-	return (dest);	
+	}
+	return (dest);
 }
