5e1ad72d55af03ba57af11e720ba439bfd7aa843
Iadolphe007 <iadolphe007@gmail.com>
2023-04-13 15:25:32 +0000

    mult positive nbr

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int is_positive_number(char* num)
+{
+	size_t i;
+
+	if (strlen(num) == 0)
+		return (0);
+	for (i = 0; i < strlen(num); i++)
+	{
+		if (!isdigit(num[i]))
+			return (0);
+	}
+	if (num[0] == '0' && strlen(num) > 1)
+		return (0);
+	return (1);
+}
+
+int main(int argc, char** argv)
+{
+	char *num1, *num2;
+	long result;
+
+	if (argc != 3)
+	{
+		fprintf(stderr, "Error\n");
+		exit(98);
+	}
+	num1 = argv[1];
+	num2 = argv[2];
+
+	if (!is_positive_number(num1) || !is_positive_number(num2))
+	{
+		fprintf(stderr, "Error\n");
+		exit(98);
+	}
+	result = atoll(num1) * atoll(num2);
+	printf("%ld\n", result);
+	return (0);
+}
