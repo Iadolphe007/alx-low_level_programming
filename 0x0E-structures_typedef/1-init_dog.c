45bdf33dc946fe6c9db8b20c8c41e238dfb27c96
Iadolphe007 <iadolphe007@gmail.com>
2023-04-17 14:56:01 +0000

    initiliaze struct var

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,8 +15,15 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = (char *) malloc(strlen(name) + 1);
 	d->owner = (char *) malloc(strlen(owner) + 1);
-	strcpy(d->name, name);
-	strcpy(d->owner, owner);
+
+	if (d->name == NULL)
+		exit (1);
+	else
+		strcpy(d->name, name);
+	if (d->owner == NULL)
+		exit (1);
+	else
+		strcpy(d->owner, owner);
 
 	d->age = age;
 
