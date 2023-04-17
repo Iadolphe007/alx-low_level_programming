f0165f56fa0b270049af2e09922b673bca398a3e
Iadolphe007 <iadolphe007@gmail.com>
2023-04-17 14:15:39 +0000

    structure header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struxture
+ * @name: structure name
+ * @owner: structure owener
+ * @age: structure age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
