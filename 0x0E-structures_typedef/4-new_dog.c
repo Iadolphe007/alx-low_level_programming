3018f42c86e4db87eeb56b326975e459fbae8c1f
Iadolphe007 <iadolphe007@gmail.com>
2023-04-17 16:42:44 +0000

    new element

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,25 +11,28 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *name_copy;
-	char *owner_copy;
+	dog_t *new_dog;
 
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (NULL);
-	name_copy = strdup(name);
-	owner_copy = strdup(owner);
 
-	if (name_copy == NULL || owner_copy == NULL)
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
-		free(name_copy);
-		free(owner_copy);
 		return (NULL);
 	}
-	new_dog->name = name_copy;
-	new_dog->owner = owner_copy;
+	new_dog->owner = strdup(owner);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
 	new_dog->age = age;
 
 	return (new_dog);
