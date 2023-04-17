8ac474b832f8fa718b336b1fab73d5e8c5cf9164
Iadolphe007 <iadolphe007@gmail.com>
2023-04-17 14:38:52 +0000

    initiliaze struct var

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog variable
+ * @d: pointer objecter
+ * @name: structure obj
+ * @age: structure oj
+ * @owner: structure obj
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = malloc(strlen(name) + 1);
+	d->owner = malloc(strlen(owner) + 1);
+	d->age = age;
+
+	strcpy(d->name, name);
+	strcpy(d->owner, owner);
+
+}
