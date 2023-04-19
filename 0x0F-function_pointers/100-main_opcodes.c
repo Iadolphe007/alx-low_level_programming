31abc8c67c9b6c74f85196bc5ff129545606743d
Iadolphe007 <iadolphe007@gmail.com>
2023-04-19 15:58:41 +0000

    opcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -20,12 +20,12 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit (1);
+		exit(1);
 	}
 	if (num_bytes < 0)
 	{
 		printf("Error\n");
-		exit (2);
+		exit(2);
 	}
 	start = (char *)&main;
 	end = start + num_bytes;
