db5cd833751e6756cf07d9664265ee0374f9467e
Iadolphe007 <iadolphe007@gmail.com>
2023-04-19 16:05:50 +0000

    opcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,30 +10,31 @@
 
 int main(int argc, char *argv[])
 {
-	int num_bytes;
-	char *start;
-	char *end;
-	char *p;
+	int i, bytes;
+	char *arr;
 
-	num_bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (num_bytes < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	start = (char *)&main;
-	end = start + num_bytes;
-	for (p = start; p < end; p++)
+	arr = (char *)main;
+	for (i = 0; i < bytes; i++)
 	{
-		printf("%02x", *p & 0xff);
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx\n", arr[i]);
 	}
-	printf("\n");
 	return (0);
 
 }
