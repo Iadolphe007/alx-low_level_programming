141706ca11e5a3967fed9f412c6224582a92d84f
Iadolphe007 <iadolphe007@gmail.com>
2023-04-20 15:44:00 +0000

    operation programm

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -10,6 +11,6 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-		return;
+		exit(98);
 	f(name);
 }
