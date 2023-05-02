5fe94067ac291dc19d4dc5bd3cc99e7a95beb180
Iadolphe007 <iadolphe007@gmail.com>
2023-05-02 17:14:55 +0000

    free fun

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees memory
+ * @head: pointer to the first node in the linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
