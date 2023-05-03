2ca622c7f1ae847561890cd81aaa5b7b83d7b31b
Iadolphe007 <iadolphe007@gmail.com>
2023-05-03 11:20:19 +0000

    print function

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * print_listint_safe - print list
+ * Return: count
+ * @head: point to the first node of a linked list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *curr = head;
+	const listint_t *loop = NULL;
+	size_t count = 0;
+
+	while (curr)
+	{
+		printf("[%p] %d\n", (void *) curr, curr->n);
+		count++;
+
+		if (curr >= curr->next)
+		{
+			if (!loop)
+			{
+				loop = curr;
+			}
+			else if (curr == loop)
+			{
+				printf("-> [%p] %d\n", (void *) curr, curr->n);
+				printf("[%p] %d\n", (void *) loop, loop->n);
+				exit(98);
+			}
+		}
+		curr = curr->next;
+	}
+	return (count);
+}
