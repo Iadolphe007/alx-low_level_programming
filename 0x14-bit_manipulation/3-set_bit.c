80b785260bfb4f7e9ccad419b9af9c4daa9345ec
Iadolphe007 <iadolphe007@gmail.com>
2023-05-04 14:33:21 +0000

    set at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * set_bit - sets the value of a bit bat index
+ * @n: first parameter
+ * @index: position to set value
+ * Return: 1 on success or -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index < sizeof(unsigned long int) * 8)
+		return (-1);
+
+	 *n |= 1UL << index;
+	return (1);
+}
