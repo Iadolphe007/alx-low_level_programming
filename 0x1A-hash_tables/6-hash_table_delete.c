94f7d5d45e886bf25892f13918e7fa114c14566e
Iadolphe007 <iadolphe007@gmail.com>
2023-07-11 07:13:46 +0000

    delete table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,22 +12,25 @@ void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node, *tmp;
 	unsigned long int i;
+	hash_table_t *head = ht;
 
 	if (ht == NULL)
 		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		node = ht->array[i];
-		while (node)
+		if (ht->array[i] != NULL)
 		{
-			tmp = node;
-			node = node->next;
-			free(tmp->key);
-			free(tmp->value);
-			free(tmp);
+			node = ht->array[i];
+			while (node)
+			{
+				tmp = node;
+				node = node->next;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+			}
 		}
-		free(ht->array);
-		free(ht);
 	}
-
+	free(head->array);
+	free(head);
 }
