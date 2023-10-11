e725c179719382a1fff2a82f774a622bd8827cbd
Iadolphe007 <iadolphe007@gmail.com>
2023-10-11 05:44:38 +0000

    time comp

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+ * binary_search - search for value using binary search
+ * @array: pointer to the first element of array
+ * @size: number of element in array
+ * @value: value to search for in array
+ * Return: index of value or -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+}
