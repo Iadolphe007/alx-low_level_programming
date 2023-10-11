e6f98804ea4abd58c556b9bd7ebcdbbc38e615ff
Iadolphe007 <iadolphe007@gmail.com>
2023-10-11 06:46:10 +0000

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,6 +9,27 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i, r, l;
 
+	if (array == NULL)
+		return (-1);
+
+	for (l = 0, r = size - 1; r >= l;)
+	{
+		printf("Searching in array: ");
+		for (i = l; i < r; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[i]);
+
+		i = l + (r - l) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
+	return (-1);
 }
